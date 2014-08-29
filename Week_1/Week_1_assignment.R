a48ee951bb6277e750989a2328091b732787e2ec
jwindokun <jwindokun@hotmail.com>
2014-08-28 21:33:34 -0400

    week_1_assignment

---
+++ Week_1/Week_1_assignment.R
@@ -0,0 +1,16 @@
+# Version of R Studio: Version 0.98.977
+# Version of R: R version 3.1.0 (2014-04-10) -- "Spring Dance"
+# Version of PostgreSQL: PostgreSQL 9.3
+#
+#
+#
+#Install the package DMwR
+
+install.packages("DMwR")
+
+#Load library
+library("DMwR")
+
+#count the number of rows in the sales dataset
+nrow(sales)
+
